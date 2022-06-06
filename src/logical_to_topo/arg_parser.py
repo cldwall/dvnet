e82858cd97234062781e8be0b2c78f63e82fd942
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-06-06 10:28:25 +0200

    Add an argument parser
    This lets us control what the code does when invoking it
    from a terminal in a natural and orderly fashion. It really
    speeds up developing!

---
+++ src/logical_to_topo/arg_parser.py
@@ -0,0 +1,42 @@
+import argparse, logging
+
+log = logging.getLogger(__name__)
+
+level_map = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
+}
+
+def _log_level_check(level):
+    if level.upper() not in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
+        raise argparse.ArgumentTypeError
+    return level_map[level.upper()]
+
+def parse_args():
+    log.debug("Parsing input arguments")
+    parser = argparse.ArgumentParser(description = "Logical to Physical topology mapper")
+    parser.add_argument(
+        "logical_definition",
+        help = "GEXF file containing the desired network's definition."
+    )
+    parser.add_argument(
+        "-r", "--remove", action = 'store_true',
+        help = "Remove the instances of elements found on <net_definition>."
+    )
+    parser.add_argument(
+        "-s", "--show", nargs = '?', default = "NOSHOW", const = "NOSTORE",
+        help = "Show a visual representation of the network defined on <net_definition> (PATH to save file)."
+    )
+    parser.add_argument(
+        "--log", nargs = '?', default = "INFO", type = _log_level_check,
+        help = "Log level (DEBUG | INFO | WARNING | ERROR | CRITICAL)."
+    )
+    parser.add_argument(
+        "-d", "--defaults", action = 'store_true',
+        help = "Show the default values for optional elements in the JSON config files."
+    )
+
+    return parser.parse_args()
