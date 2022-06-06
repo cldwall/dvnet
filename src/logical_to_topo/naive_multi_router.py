943e55e0355886dc9ec9deca32d25d452f52c3d3
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-06-06 13:27:38 +0200

    Break network instantiation down into steps
    We're now leveraging several functions allowing us
    to better control how networks get instantiated!

--- src/logical_to_topo/naive_multi_router.py
+++ src/logical_to_topo/naive_multi_router.py
@@ -15,79 +15,19 @@ log = logging.getLogger(__name__)
 
 def instantiate_net(logicalGraph):
     ni._system_setup()
-
     currentSubnet = "10.0.0.0/30"
-
-    topology, n = nx.Graph(name = "Topology"), len(logicalGraph)
-
-    log.info(f"Adding bridge brdCore to the topology graph")
+    topology, nNodes = nx.Graph(name = "Topology"), len(logicalGraph)
     topology.add_node("brdCore", type = "bridge", subnet = "")
     ni._create_bridge("brdCore")
 
-    for i, node in enumerate(list(logicalGraph.nodes())):
-        brdName, hostName, routerName = f"brd{i}", f"h{i}", f"r{i}"
-
-        log.info(f"Adding bridge {brdName} to the topology graph")
-        topology.add_node(brdName, type = "bridge", subnet = "")
-
-        ni._create_bridge(brdName)
-
-        log.info(f"Adding host {hostName} to the topology graph")
-        topology.add_node(hostName, type = "host")
-
-        log.info(f"Instantiating host {hostName}")
-        ni._create_node(hostName, dx.types.host, "pcollado/d_host")
-
-        log.info(f"Adding the {brdName} <--> {hostName} edge")
-        topology.add_edge(brdName, hostName)
-
-        nIface, _ = ni._connect_node(hostName, brdName)
-
-        hIP = addr_manager.request_ip(currentSubnet, hname = hostName)
-        ipaddr.assign(nIface, hIP, netns = hostName)
-
-        log.info(f"Adding router {routerName} to the topology graph")
-        topology.add_node(routerName, type = "router", internet_gw = False)
-
-        ni._create_node(routerName, dx.types.router, "pcollado/d_router")
-        log.info(f"Adding the {brdName} <--> {routerName} edge")
-        topology.add_edge(brdName, routerName)
-        rIfaceSubnet, _ = ni._connect_node(routerName, brdName)
-        log.info(f"Adding the brdCore <--> {routerName} edge")
-        topology.add_edge("brdCore", routerName)
-        rIfaceCore, _ = ni._connect_node(routerName, "brdCore")
-
-        rIPSubnet = addr_manager.request_ip(currentSubnet, hname = routerName)
-        rIPCore = addr_manager.request_ip("172.16.0.0/12", hname = routerName)
-        ipaddr.assign(rIfaceSubnet, rIPSubnet, netns = routerName)
-        ipaddr.assign(rIfaceCore, rIPCore, netns = routerName)
-
-        iproute.assign("default", rIPSubnet.split("/")[0], netns = hostName)
+    for i in range(nNodes):
+        addHost(topology, i, currentSubnet)
 
         currentSubnet = "{}/30".format(
             addr_manager.binary_to_addr(ip_utils.addr_to_binary(currentSubnet.split("/")[0]) + 4)
         )
-
-    rNames = [f"r{i}" for i in range(n)]
-    for tRouter in rNames:
-        rawSubnet = addr_manager.name_2_ip(tRouter, -1)
-        tSubnet = "{}/{}".format(
-            addr_manager.binary_to_addr(addr_manager.get_net_addr(rawSubnet[0])),
-            rawSubnet[0].split('/')[1]
-        )
-        tIP = rawSubnet[-1].split('/')[0]
-        for sRouter in rNames:
-            if sRouter == tRouter:
-                continue
-            iproute.assign(tSubnet, tIP, netns = sRouter)
-
-    relabeledLogicalGraph = nx.relabel_nodes(logicalGraph,
-        {node: f"h{i}" for i, node in enumerate(logicalGraph.nodes())})
-
-    for i, node in enumerate(relabeledLogicalGraph):
-        dx.apply_fw_rules(f"r{i}", {"POLICY": "DROP", "DROP": [], "ACCEPT": [
-            (node, neigh, True) for neigh in relabeledLogicalGraph.neighbors(node)
-        ]})
+    routerNetwork(nNodes)
+    configureFirewalls(logicalGraph)
 
 def remove_net(logicalGraph):
     tmp = {"bridges": ["brdCore"], "containers": []}
@@ -105,13 +45,7 @@ def dump_graph_figure(logicalGraph, name: str):
     topology.add_node("brdCore", type = "bridge", subnet = "")
 
     for i in range(n):
-        brdName, hostName, routerName = f"brd{i}", f"h{i}", f"r{i}"
-        topology.add_node(brdName, type = "bridge", subnet = "")
-        topology.add_node(hostName, type = "host")
-        topology.add_edge(brdName, hostName)
-        topology.add_node(routerName, type = "router", internet_gw = False)
-        topology.add_edge(brdName, routerName)
-        topology.add_edge("brdCore", routerName)
+        addGraphNode(f"brd{i}", f"h{i}", f"r{i}")
 
     relabeledLogicalGraph = nx.relabel_nodes(logicalGraph,
         {node: f"h{i}" for i, node in enumerate(logicalGraph.nodes())})
@@ -122,3 +56,64 @@ def dump_graph_figure(logicalGraph, name: str):
 
     nx.write_gexf(relabeledLogicalGraph, f"{name}_relabeled.gexf")
     net_visualization.show_net(relabeledLogicalGraph, f"{name}_relabeled")
+
+def addHost(graph, id, subnet):
+    brdName, hostName, routerName = f"brd{id}", f"h{id}", f"r{id}"
+    addGraphNode(graph, brdName, hostName, routerName)
+    hIface, rIfaceSubnet, rIfaceCore = addNetworkInfrastructure(brdName, hostName, routerName)
+    routerSubnetIP = addNetworkAddresses(
+        [(subnet, hostName, hIface), (subnet, routerName, rIfaceSubnet), ("172.16.0.0/12", routerName, rIfaceCore)]
+    )
+    addHostNetworkRoutes(hostName, routerSubnetIP)
+
+def addGraphNode(graph, bridge, host, router):
+    graph.add_node(bridge, type = "bridge", subnet = "")
+    graph.add_node(host, type = "host")
+    graph.add_edge(bridge, host)
+    graph.add_node(router, type = "router", internet_gw = False)
+    graph.add_edge(bridge, router)
+    graph.add_edge("brdCore", router)
+
+def addNetworkInfrastructure(bridge, host, router):
+    ni._create_bridge(bridge)
+    ni._create_node(host, dx.types.host, "pcollado/d_host")
+    hIface, _ = ni._connect_node(host, bridge)
+    ni._create_node(router, dx.types.router, "pcollado/d_router")
+    rIfaceSubnet, _ = ni._connect_node(router, bridge)
+    rIfaceCore, _ = ni._connect_node(router, "brdCore")
+
+    return hIface, rIfaceSubnet, rIfaceCore
+
+def addNetworkAddresses(addressingInfo):
+    for subnet, nodeName, iface in addressingInfo:
+        reqIP = addr_manager.request_ip(subnet, hname = nodeName)
+        if nodeName.startswith('r') and not subnet.startswith("172"):
+            routerSubnetIP = reqIP
+        ipaddr.assign(iface, reqIP, netns = nodeName)
+    return routerSubnetIP
+
+def addHostNetworkRoutes(host, routerSubnetIP):
+    iproute.assign("default", routerSubnetIP.split("/")[0], netns = host)
+
+def routerNetwork(nNodes):
+    rNames = [f"r{i}" for i in range(nNodes)]
+    for tRouter in rNames:
+        rawSubnet = addr_manager.name_2_ip(tRouter, -1)
+        tSubnet = "{}/{}".format(
+            addr_manager.binary_to_addr(addr_manager.get_net_addr(rawSubnet[0])),
+            rawSubnet[0].split('/')[1]
+        )
+        tIP = rawSubnet[-1].split('/')[0]
+        for sRouter in rNames:
+            if sRouter == tRouter:
+                continue
+            iproute.assign(tSubnet, tIP, netns = sRouter)
+
+def configureFirewalls(logicalGraph):
+    relabeledLogicalGraph = nx.relabel_nodes(logicalGraph,
+        {node: f"h{i}" for i, node in enumerate(logicalGraph.nodes())})
+
+    for i, node in enumerate(relabeledLogicalGraph):
+        dx.apply_fw_rules(f"r{i}", {"POLICY": "DROP", "DROP": [], "ACCEPT": [
+            (node, neigh, True) for neigh in relabeledLogicalGraph.neighbors(node)
+        ]})
