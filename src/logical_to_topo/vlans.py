f875742402b205d1fa13dee4158358a1716740ef
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-06-09 19:05:02 +0200

    Shorten bridge names
    The `iproute2` suite imposes strict limits on the length
    of interface names...

--- src/logical_to_topo/vlans.py
+++ src/logical_to_topo/vlans.py
@@ -20,19 +20,19 @@ def instantiate_net(logicalGraph, storeCliques):
     ni._system_setup()
 
     topology = nx.Graph(name = "Topology")
-    topology.add_node("brdCore", type = "bridge", subnet = "")
-    ni._create_bridge("brdCore")
+    topology.add_node("brdC", type = "bridge", subnet = "")
+    ni._create_bridge("brdC")
 
     trunkVLAN = ipvlan.vlan(0)
 
-    topology.add_node("brdEdge0", type = "bridge", subnet = "")
-    topology.add_edge("brdCore", "brdEdge0")
-    ni._create_bridge("brdEdge0")
-    iplink.bridge.enableVLAN("brdEdge0")
-    _, edgeIface = ni._connect_node("brdCore", "brdEdge0")
+    topology.add_node("brdE0", type = "bridge", subnet = "")
+    topology.add_edge("brdC", "brdE0")
+    ni._create_bridge("brdE0")
+    iplink.bridge.enableVLAN("brdE0")
+    _, edgeIface = ni._connect_node("brdC", "brdE0")
     trunkVLAN.addIface(edgeIface)
 
-    nextFreeIP, currVLANID, instantiatedHosts, currEdgeBridge = "10.0.0.0", 2, 0, "brdEdge0"
+    nextFreeIP, currVLANID, instantiatedHosts, currEdgeBridge = "10.0.0.0", 2, 0, "brdE0"
 
     for clique in nx.find_cliques(logicalGraph):
 
@@ -62,18 +62,18 @@ def instantiate_net(logicalGraph, storeCliques):
                 # we need a remaining one for a trunk port. We can use the following to effectively
                 # check that's the imposed limit: `echo $((( $(bridge -c vlan show | wc -l) - 1) / 2))`
             if instantiatedHosts % 1023 == 0:
-                currEdgeBridge = f"brdEdge{int(instantiatedHosts / 1023)}"
+                currEdgeBridge = f"brdE{int(instantiatedHosts / 1023)}"
                 topology.add_node(currEdgeBridge, type = "bridge", subnet = "")
-                topology.add_edge("brdCore", currEdgeBridge)
+                topology.add_edge("brdC", currEdgeBridge)
                 ni._create_bridge(currEdgeBridge)
                 iplink.bridge.enableVLAN(currEdgeBridge)
-                _, edgeIface = ni._connect_node("brdCore",currEdgeBridge)
+                _, edgeIface = ni._connect_node("brdC",currEdgeBridge)
                 trunkVLAN.addIface(edgeIface)
 
         log.debug(f"Assigned addresses -> {addr_manager.assigned_addreses}")
 
 def remove_net(logicalGraph):
-    tmp = {"bridges": ["brdCore"], "containers": []}
+    tmp = {"bridges": ["brdC", *[f"brdE{i}" for i in range(len(logicalGraph) / 1023)]], "containers": []}
     for node in logicalGraph:
         tmp["containers"].append(node)
 
@@ -83,7 +83,7 @@ def remove_net(logicalGraph):
 def dump_graph_figure(logicalGraph, name: str):
     topology = nx.MultiGraph(name = f"{name.capitalize().replace('_', ' ')} Topology")
 
-    topology.add_node("brdCore", type = "bridge", subnet = "")
+    topology.add_node("brdC", type = "bridge", subnet = "")
 
     for clique in nx.find_cliques(logicalGraph):
         log.warn(f"Taking clique {clique} into account")
@@ -91,23 +91,23 @@ def dump_graph_figure(logicalGraph, name: str):
         for host in clique:
             if host not in topology:
                 topology.add_node(host, type = "host")
-            topology.add_edge(host, "brdCore")
+            topology.add_edge(host, "brdC")
 
     nx.write_gexf(topology, f"{name}_topology.gexf")
     write_dot(topology, f"{name}_topology.dot")
 
     net_visualization.show_net(topology, f"{name}_topology")
 
-def addHost(graph, hostName, subnet, vlan):
+def addHost(graph, brdName, hostName, subnet, vlan):
     if hostName not in graph:
         graph.add_node(hostName, type = "host")
         ni._create_node(hostName, dx.types.host, "pcollado/d_host")
-    graph.add_edge(hostName, "brdCore")
-    hIface = addNetworkInfrastructure(hostName, vlan)
+    graph.add_edge(hostName, brdName)
+    hIface = addNetworkInfrastructure(brdName, hostName, vlan)
     addNetworkAddresses(subnet, hostName, hIface)
 
-def addNetworkInfrastructure(host, vlan: ipvlan.vlan):
-    hIface, brdIface = ni._connect_node(host, "brdCore", vID = vlan.vID)
+def addNetworkInfrastructure(brdName, host, vlan: ipvlan.vlan):
+    hIface, brdIface = ni._connect_node(host, brdName, vID = vlan.vID)
     vlan.addIface(brdIface)
     return hIface
 
