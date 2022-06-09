df7ff8d8e0e2baae5aa7dd5e85391daa0d9fd121
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-06-09 14:09:00 +0200

    Alter the way cliques are found
    We're swallowing up 62 GiB of RAM!

--- src/logical_to_topo/vlans.py
+++ src/logical_to_topo/vlans.py
@@ -24,56 +24,32 @@ def instantiate_net(logicalGraph, storeCliques):
     ni._create_bridge("brdCore")
     iplink.bridge.enableVLAN("brdCore")
 
-    log.debug("Beginning clique discovery...")
-    cliques = discoverCliques(logicalGraph, storeCliques)
-    log.debug(f"Discovered cliques: {cliques}")
-
     nextFreeIP, currVLANID = "10.0.0.0", 2
 
-    for cLen, cliqueL in cliques.items():
-        if cLen == 0:
-            continue
-        for clique in cliqueL:
-            if discardClique(clique, cLen, cliques):
-                continue
+    for clique in nx.find_cliques(logicalGraph):
 
-            log.info(f"Taking clique {clique} into account")
+        log.info(f"Taking clique {clique} into account")
 
-            cliqueSubnetIPsExp = math.ceil(math.log(len(clique) + 2, 2))
+        cliqueSubnetIPsExp = math.ceil(math.log(len(clique) + 2, 2))
 
-            binNextFreeIP = addr_manager.addr_to_binary(nextFreeIP)
-            if binNextFreeIP % (2 ** cliqueSubnetIPsExp) != 0:
-                binNextFreeIP += (2 ** cliqueSubnetIPsExp) - (binNextFreeIP % (2 ** cliqueSubnetIPsExp))
-                nextFreeIP = addr_manager.binary_to_addr(binNextFreeIP)
+        binNextFreeIP = addr_manager.addr_to_binary(nextFreeIP)
+        if binNextFreeIP % (2 ** cliqueSubnetIPsExp) != 0:
+            binNextFreeIP += (2 ** cliqueSubnetIPsExp) - (binNextFreeIP % (2 ** cliqueSubnetIPsExp))
+            nextFreeIP = addr_manager.binary_to_addr(binNextFreeIP)
 
-            cliqueSubnet = f"{nextFreeIP}/{32 - cliqueSubnetIPsExp}"
+        cliqueSubnet = f"{nextFreeIP}/{32 - cliqueSubnetIPsExp}"
 
-            nextFreeIP = addr_manager.binary_to_addr(addr_manager.addr_to_binary(nextFreeIP) + 2 ** cliqueSubnetIPsExp)
+        nextFreeIP = addr_manager.binary_to_addr(addr_manager.addr_to_binary(nextFreeIP) + 2 ** cliqueSubnetIPsExp)
 
-            cliqueVLAN = ipvlan.vlan(currVLANID)
-            currVLANID += 1
+        cliqueVLAN = ipvlan.vlan(currVLANID)
+        currVLANID += 1
 
-            log.debug(f"Assigning addresses from subnet --> {cliqueSubnet}")
+        log.debug(f"Assigning addresses from subnet --> {cliqueSubnet}")
 
-            for host in clique:
-                addHost(topology, host, cliqueSubnet, cliqueVLAN)
+        for host in clique:
+            addHost(topology, host, cliqueSubnet, cliqueVLAN)
 
         log.debug(f"Assigned addresses -> {addr_manager.assigned_addreses}")
-
-def discoverCliques(logicalGraph: nx.Graph, storeCliques: bool = False) -> dict[int, Union[int, list[set]]]:
-    cliques = {}
-    for clique in list(nx.enumerate_all_cliques(logicalGraph)):
-        cLen = len(clique)
-        if cLen <= 1:
-            continue
-        if cLen not in cliques:
-            cliques[cLen] = [set(clique)]
-        else:
-            cliques[cLen].append(set(clique))
-        cliques[0] = max(cliques.keys())
-    if storeCliques:
-        pathlib.Path("cliques.json").write_text(json.dumps(cliques))
-    return cliques
 
 def remove_net(logicalGraph):
     tmp = {"bridges": ["brdCore"], "containers": []}
@@ -88,34 +64,18 @@ def dump_graph_figure(logicalGraph, name: str):
 
     topology.add_node("brdCore", type = "bridge", subnet = "")
 
-    cliques = discoverCliques(logicalGraph)
+    for clique in nx.find_cliques(logicalGraph):
+        log.warn(f"Taking clique {clique} into account")
 
-    for cLen, cliqueL in cliques.items():
-        if cLen == 0:
-            continue
-        for clique in cliqueL:
-            if discardClique(clique, cLen, cliques):
-                continue
-
-            log.warn(f"Taking clique {clique} into account")
-
-            for host in clique:
-                if host not in topology:
-                    topology.add_node(host, type = "host")
-                topology.add_edge(host, "brdCore")
+        for host in clique:
+            if host not in topology:
+                topology.add_node(host, type = "host")
+            topology.add_edge(host, "brdCore")
 
     nx.write_gexf(topology, f"{name}_topology.gexf")
     write_dot(topology, f"{name}_topology.dot")
 
     net_visualization.show_net(topology, f"{name}_topology")
-
-def discardClique(clique, cLen, cliques):
-    for l in range(cLen + 1, cliques[0] + 1):
-        for bClique in cliques[l]:
-            if bClique >= clique:
-                log.debug(f"Clique {clique} is a subset of {bClique}! Discarding it...")
-                return True
-    return False
 
 def addHost(graph, hostName, subnet, vlan):
     if hostName not in graph:
